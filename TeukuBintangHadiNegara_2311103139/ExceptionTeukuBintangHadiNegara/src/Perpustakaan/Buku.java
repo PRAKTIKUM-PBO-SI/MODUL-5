926f5674e6cd507d277cf57d17b765b9575c609c
Teuku Bintang Hadi Negara <vasgarddragunov@gmail.com>
2024-11-28 23:24:22 +0700

    Add files via upload

---
+++ TeukuBintangHadiNegara_2311103139/ExceptionTeukuBintangHadiNegara/src/Perpustakaan/Buku.java
@@ -0,0 +1,77 @@
+package Perpustakaan;
+
+/**
+ *
+ * @author Teuku Bintang Hadi Negara
+ */
+public class Buku {
+    private String judul;
+    private String pengarang;
+    private String isbn;
+    private String tahunTerbit;
+    private String harga;
+    private String kategori;
+    
+    public Buku(String judul, String pengarang, String isbn, String tahunTerbit, String harga, String kategori) throws ValidasiInputException {
+    if (judul.isEmpty()) {
+        throw new ValidasiInputException("Judul buku tidak boleh kosong!");
+    }
+    
+    if (pengarang.isEmpty()) {
+        throw new ValidasiInputException("Pengarang tidak boleh kosong!");
+    }
+    
+    if (!isbn.matches("\\d{13}")) {
+        throw new ValidasiInputException("ISBN harus 13 digit angka!");
+    }
+    
+    if (!tahunTerbit.matches("\\d{4}")) {
+        throw new ValidasiInputException("Tahun Terbit harus 4 digit angka!");
+    }
+    
+    try {
+        long hargaNumber = Long.parseLong(harga); // Ubah ISBN menjadi angka
+        if (hargaNumber < 0) {
+            throw new ValidasiInputException("Harga tidak boleh angka negatif!");
+        }
+    } catch (NumberFormatException e) {
+        throw new ValidasiInputException("Input harus berupa angka!");
+    }
+    
+    if (kategori == null || kategori.isEmpty()) {
+        throw new ValidasiInputException("Kategori harus dipilih!");
+    }
+
+    this.judul = judul;
+    this.pengarang = pengarang;
+    this.isbn = isbn;
+    this.tahunTerbit = tahunTerbit;
+    this.harga = harga;
+    this.kategori = kategori;
+    
+    }
+    
+    public String getJudul() {
+        return judul;
+    }
+    
+    public String getPengarang() {
+        return pengarang;
+    }
+    
+    public String getIsbn() {
+        return isbn;
+    }
+    
+    public String getTahunTerbit() {
+        return tahunTerbit;
+    }
+    
+    public String getHarga() {
+        return harga;
+    }
+    
+    public String getKategori() {
+        return kategori;
+    }
+}
---
+++ TeukuBintangHadiNegara_2311103139/ExceptionTeukuBintangHadiNegara/src/Perpustakaan/ValidasiInputException.java
@@ -0,0 +1,11 @@
+package Perpustakaan;
+
+/**
+ *
+ * @author Teuku Bintang Hadi Negara
+ */
+public class ValidasiInputException extends Exception {
+    public ValidasiInputException(String message) {
+        super(message);
+    }
+}
