e5666de57c7a3f9dc290b3fa096c844d1dc9642d
Raditya <91859331+radityaputra1@users.noreply.github.com>
2024-11-30 08:08:22 +0700

    Add files via upload

---
+++ Raditya Putra Anugra Pratama_2311103143_Modul 5/ValidasiInputException.java
@@ -0,0 +1,11 @@
+package Perpustakaan;
+
+/**
+ *
+ * @author Raditya Putra Anugra Pratama
+ */
+public class ValidasiInputException extends Exception{
+    public ValidasiInputException(String message) {
+        super(message);
+    }
+}
