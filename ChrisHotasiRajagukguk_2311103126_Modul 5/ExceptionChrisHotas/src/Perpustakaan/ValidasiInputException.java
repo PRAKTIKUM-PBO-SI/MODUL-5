44cd429d32b73cff1ac4f1ca0dc93d3a816366d4
chrisshtss17 <chrishotasi697@gmail.com>
2024-12-02 04:58:33 +0700

    Add files via upload

---
+++ ChrisHotasiRajagukguk_2311103126_Modul 5/ExceptionChrisHotas/src/Perpustakaan/ValidasiInputException.java
@@ -0,0 +1,9 @@
+package Perpustakaan;
+/**
+ * @author Chris Hotasi
+*/
+public class ValidasiInputException extends Exception {
+    public ValidasiInputException(String message) {
+        super(message);
+    }
+}
