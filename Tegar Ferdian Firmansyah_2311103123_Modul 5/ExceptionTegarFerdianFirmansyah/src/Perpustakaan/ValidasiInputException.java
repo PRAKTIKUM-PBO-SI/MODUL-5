486df898d18bed7e22b4ed758ebcec04060e5041
tegarfrdian <tegarferdian648@gmail.com>
2024-12-02 04:37:07 +0700

    Add files via upload

---
+++ Tegar Ferdian Firmansyah_2311103123_Modul 5/ExceptionTegarFerdianFirmansyah/src/Perpustakaan/ValidasiInputException.java
@@ -0,0 +1,9 @@
+package Perpustakaan;
+/**
+ * @author Tegar Ferdian Firmansyah
+*/
+public class ValidasiInputException extends Exception {
+    public ValidasiInputException(String message) {
+        super(message);
+    }
+}
