27267da632d5d2c1aadd81a5965ae0eabd0fe939
ARDZA <91511294+ArdzaRicho@users.noreply.github.com>
2024-12-01 21:48:52 +0700

    Add files via upload

---
+++ Ardzaricho R_2311103137_Modul 5/ExceptionArdzarichoRevansyah/src/Perpustakaan/ValidasiInputException.java
@@ -0,0 +1,11 @@
+package Perpustakaan;
+/**
+ * @author Ardzaricho Revansyah
+ * 2311103137
+ * S1SI-07-c
+ */
+public class ValidasiInputException extends Exception {
+    public ValidasiInputException(String message) {
+        super(message);
+    }
+}
